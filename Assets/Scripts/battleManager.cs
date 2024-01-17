3e96787f3c1c6b2483148094df8fa94fa07f85c0
rain athkravisoonthorn <107684386+areain10@users.noreply.github.com>
2024-01-17 11:11:02 -0800

    enemy has basic AI, smooth camera, death logic

--- Assets/Scripts/battleManager.cs
+++ Assets/Scripts/battleManager.cs
@@ -2,44 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public enum battleState {  START, PLAYERTURN, TARGETTING, ENEMYTURN, WON,LOST}
 public class battleManager : MonoBehaviour
 {
+    //Player Characters
     public GameObject[] player;
-    public GameObject[] enemy;
-
-    public battleState state;
-    public specialTypes specials;
     public Transform[] playerSpawn;
+    List<Character> playerCharacter = new List<Character>();
+    private Character tempPlayer;
+
+    //Enemy Character
+    public GameObject[] enemy;
     public Transform[] enemySpawn;
     List<Character> enemyCharacter = new List<Character>();
-    /*Character[] playerCharacter;*/
-    List<Character> playerCharacter = new List<Character>();
+    private Character tempEnemy;
+
+
     public int currentCharacter = 0;
-    // Start is called before the first frame update
+
+    //ENUMS
+    public battleState state;
+    public specialTypes specials;
+    
+
+    // Energy Management
     public int maxEnergy;
     public int team1Energy;
     public int team2Energy;
-    [SerializeField] public Slider team1EnergySlider;
+    
 
-    [SerializeField] public Text battleStatusText;
-
+    //Camera
     [SerializeField] public Camera mainCamera;
     [SerializeField] public Transform ogCamPos;
     [SerializeField] public Transform targetCamOffset;
+    [SerializeField] public Transform playerCamOffset;
 
+
+    //UI Elements
     [SerializeField] public Image playerUI;
     [SerializeField] public Image targettingUI;
+    [SerializeField] public Slider team1EnergySlider;
+    [SerializeField] public Text battleStatusText;
 
+    // ATTACKS
     int target = 0;
     int typeOfAttack = 1;
 
+    //Random
+  
 
-
+/*-----------Start/Setup------------------*/
     void Start()
     {
         state = battleState.START;
@@ -48,12 +66,11 @@ public class battleManager : MonoBehaviour
         team1EnergySlider.maxValue = maxEnergy;
         UpdateSliders();
         StartCoroutine(BattleSetup());
+        
 
     }
-    private void UpdateSliders()
-    {
-        team1EnergySlider.value = team1Energy;
-    }
+    
+
     IEnumerator BattleSetup()
     {
         battleStatusText.text = "Setting up Battle";
@@ -76,21 +93,22 @@ public class battleManager : MonoBehaviour
         
     }
 
-    void targettingCamera()
-    {
-        mainCamera.transform.position = enemyCharacter[target].transform.position + targetCamOffset.position;
-    }
+
+ /*-----------Player Turn------------------*/
+
     void PlayerTurn()
     {
         target = 0;
         battleStatusText.text = player[currentCharacter].name.ToString() + "'s Turn";
-        mainCamera.transform.position = playerSpawn[currentCharacter].transform.GetChild(0).transform.position;
+        mainCamera.transform.position = playerSpawn[currentCharacter].transform.position + playerCamOffset.position;
         playerUI.gameObject.SetActive(true);
         targettingUI.gameObject.SetActive(false);
         Debug.Log("Player Turn");
     }
 
+    
 
+    /*----------Buttons----------*/
     public void OnAttackButton()
     {
         if(state != battleState.PLAYERTURN)
@@ -119,10 +137,12 @@ public class battleManager : MonoBehaviour
 
     }
 
+    /*----------Targetting----------*/
+
     void Targeting()
     {
         battleStatusText.text = "Targetting " + enemyCharacter[target].name.ToString();
-        targettingCamera();
+        StartCoroutine(targettingCamera());
         playerUI.gameObject.SetActive(false);
         targettingUI.gameObject.SetActive(true);
     }
@@ -138,7 +158,7 @@ public class battleManager : MonoBehaviour
             target = temp;
         }
         battleStatusText.text = "Targetting " + enemyCharacter[target].name.ToString();
-        targettingCamera();
+        StartCoroutine(targettingCamera());
     }
 
     public void OnPreviousTargetButton()
@@ -153,7 +173,7 @@ public class battleManager : MonoBehaviour
             target = temp;
         }
         battleStatusText.text = "Targetting " + enemyCharacter[target].name.ToString();
-        targettingCamera();
+        StartCoroutine(targettingCamera());
     }
     public void OnConfirmAttackButton()
     {
@@ -180,66 +200,48 @@ public class battleManager : MonoBehaviour
 
         }
     }
-    IEnumerator EnemyTurn()
-    {
-        battleStatusText.text = "Enemy Attacks";
-        mainCamera.transform.position = ogCamPos.position;
-        Debug.Log("EnemyTurn");
-        bool isdead = playerCharacter[0].takeDamage(enemyCharacter[0].damage);
-        yield return new WaitForSeconds(2f);
-
-        if (isdead)
-        {
-            state = battleState.LOST;
-            EndBattle();
-
-        }
-        state = battleState.PLAYERTURN;
-        currentCharacter = 0;
-        removeAllBuffsFromPlayer();
-        PlayerTurn();
-    }
-
-    IEnumerator PlayerAttack(int attack,int enemy)
+    /*----------Player Attack*----------*/
+    IEnumerator PlayerAttack(int attack, int enemy)
     {
         targettingUI.gameObject.SetActive(false);
         bool isdead = false;
-        
+        tempPlayer = playerCharacter[currentCharacter];
+        tempEnemy = enemyCharacter[enemy];
         switch (attack)
         {
-            case 1: isdead = enemyCharacter[enemy].takeDamage(playerCharacter[currentCharacter].damage); battleStatusText.text = "Player attacks " + attack.ToString(); break;
-                case 2:
+            case 1: isdead = tempEnemy.takeDamage(tempPlayer.damage); battleStatusText.text = "Player attacks " + attack.ToString(); break;
+            case 2:
                 switch (playerCharacter[currentCharacter].GetSpecialTypes())
                 {
                     case specialTypes.DAMAGE:
-                        isdead = enemyCharacter[enemy].takeDamage(playerCharacter[currentCharacter].gameObject.GetComponent<Warrior>().specialDmg);
-                        battleStatusText.text = playerCharacter[currentCharacter].name.ToString() + " Special Attacks " + enemyCharacter[enemy].name.ToString();
+                        isdead = tempEnemy.takeDamage(tempPlayer.gameObject.GetComponent<Warrior>().specialDmg);
+                        battleStatusText.text = tempPlayer.name.ToString() + " Special Attacks " + tempEnemy.name.ToString();
                         break;
                     case specialTypes.DEBUFFDEFENSE:
-                        enemyCharacter[enemy].debuffDefence(playerCharacter[currentCharacter].gameObject.GetComponent<Debuffer>().debuffMultiplier);
-                        battleStatusText.text = playerCharacter[currentCharacter].name.ToString()+" debuff " + enemyCharacter[enemy].name.ToString();
+                        tempEnemy.debuffDefence(tempPlayer.gameObject.GetComponent<Debuffer>().debuffMultiplier);
+                        battleStatusText.text = tempPlayer.name.ToString() + " debuff " + tempEnemy.name.ToString();
                         break;
                     case specialTypes.HEAL:
                         float lowestHealth = 9999;
                         int index = 0;
-                        for (int i = 0;i<playerCharacter.Count;i++)
+                        for (int i = 0; i < playerCharacter.Count; i++)
                         {
                             float healthratio = playerCharacter[i].health / playerCharacter[i].maxHealth;
                             if (healthratio < lowestHealth)
                             {
-                                
+
                                 lowestHealth = healthratio;
                                 Debug.Log(lowestHealth);
                                 index = i;
                             }
                         }
                         mainCamera.transform.position = playerSpawn[index].transform.GetChild(0).transform.position;
-                        playerCharacter[index].heal(playerCharacter[currentCharacter].gameObject.GetComponent<Healer>().Healing);
-                        battleStatusText.text = playerCharacter[currentCharacter].name.ToString() + " heals " + playerCharacter[index].ToString();
+                        playerCharacter[index].heal(tempPlayer.gameObject.GetComponent<Healer>().Healing);
+                        battleStatusText.text = tempPlayer.name.ToString() + " heals " + playerCharacter[index].ToString();
                         break;
                 }
                 break;
-                
+
         }
         Debug.Log(isdead);
         UpdateSliders();
@@ -247,30 +249,153 @@ public class battleManager : MonoBehaviour
 
         if (isdead)
         {
+            enemyCharacter[enemy].gameObject.SetActive(false);
+            enemyCharacter.RemoveAt(enemy);
+
+
+        }
+        if(enemyCharacter.Count <= 0)
+        {
             state = battleState.WON;
             EndBattle();
+        }
+        
+        currentCharacter += 1;
+        Debug.Log(currentCharacter);
+        if (currentCharacter >= playerCharacter.Count)
+        {
+            state = battleState.ENEMYTURN;
 
-
+            currentCharacter = 0;
+            StartCoroutine(EnemyTurn());
         }
         else
         {
-
-
-            currentCharacter += 1;
-            Debug.Log(currentCharacter);
-            if (currentCharacter >= playerCharacter.Count)
-            {
-                state = battleState.ENEMYTURN;
-
-
-                StartCoroutine(EnemyTurn());
-            }
-            else
-            {
-                state = battleState.PLAYERTURN;
-                PlayerTurn();
-            }
+            state = battleState.PLAYERTURN;
+            PlayerTurn();
         }
+        
+    }
+
+/*---------------------------ENEMY TURN-----------------------------------*/
+    IEnumerator EnemyTurn()
+    {
+        target = UnityEngine.Random.Range(0,playerCharacter.Count);
+        battleStatusText.text = enemyCharacter[currentCharacter].name.ToString() + "'s Turn";
+        mainCamera.transform.position = enemyCharacter[currentCharacter].transform.position + targetCamOffset.position;
+        Debug.Log("EnemyTurn");
+        
+        yield return new WaitForSeconds(2f);
+        mainCamera.transform.position = playerCharacter [target].transform.position + targetCamOffset.position;
+        StartCoroutine(EnemyAttack(UnityEngine.Random.Range(1,3), target));
+    }
+
+    IEnumerator EnemyAttack(int attack, int player)
+    {
+        Debug.Log("Attack:" + attack + " Player: " + 1);
+        bool isdead = false;
+        tempEnemy = enemyCharacter[currentCharacter];
+        tempPlayer = playerCharacter[player];
+        switch (attack)
+        {
+            case 1: isdead = tempPlayer.takeDamage(tempEnemy.damage); battleStatusText.text = tempEnemy.name.ToString() + "attacks " + playerCharacter[player].ToString(); break;
+            case 2:
+                switch (tempEnemy.GetSpecialTypes())
+                {
+                    case specialTypes.DAMAGE:
+                        isdead = tempPlayer.takeDamage(tempEnemy.gameObject.GetComponent<Warrior>().specialDmg);
+                        battleStatusText.text = tempEnemy.name.ToString() + " Special Attacks " + tempPlayer.name.ToString();
+                        break;
+                    case specialTypes.DEBUFFDEFENSE:
+                        tempPlayer.debuffDefence(tempEnemy.gameObject.GetComponent<Debuffer>().debuffMultiplier);
+                        battleStatusText.text = tempEnemy.name.ToString() + " debuff " + tempPlayer.name.ToString();
+                        break;
+                    case specialTypes.HEAL:
+                        float lowestHealth = 9999;
+                        int index = 0;
+                        for (int i = 0; i < enemyCharacter.Count; i++)
+                        {
+                            float healthratio = enemyCharacter[i].health / enemyCharacter[i].maxHealth;
+                            if (healthratio < lowestHealth)
+                            {
+
+                                lowestHealth = healthratio;
+                                Debug.Log(lowestHealth);
+                                index = i;
+                            }
+                        }
+                        mainCamera.transform.position = playerSpawn[index].transform.GetChild(0).transform.position;
+                        enemyCharacter[index].heal(tempEnemy.gameObject.GetComponent<Healer>().Healing);
+                        battleStatusText.text = tempEnemy.name.ToString() + " heals " + enemyCharacter[index].ToString();
+                        break;
+                }
+                break;
+
+        }
+        Debug.Log(isdead);
+        UpdateSliders();
+        yield return new WaitForSeconds(2f);
+
+        if (isdead)
+        {
+            playerCharacter[player].gameObject.SetActive(false);
+            playerCharacter.RemoveAt(player);
+
+
+        }
+        if (enemyCharacter.Count <= 0)
+        {
+            state = battleState.LOST;
+            EndBattle();
+        }
+        
+        
+
+
+        currentCharacter += 1;
+            
+        if (currentCharacter >= enemyCharacter.Count)
+        {
+            state = battleState.PLAYERTURN;
+
+            currentCharacter = 0;
+            PlayerTurn();
+        }
+        else
+        {
+            state = battleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+        
+    }
+
+
+
+
+
+
+
+    /*--------------Functions------------*/
+    private void UpdateSliders()
+    {
+        team1EnergySlider.value = team1Energy;
+    }
+
+    IEnumerator targettingCamera()
+    {
+        float totalMovementTime = 0.5f;
+        float currentMovementTime = 0f;
+        Vector3 startPos = mainCamera.transform.position;
+        Vector3 destination = enemyCharacter[target].transform.position + targetCamOffset.position;
+        while (currentMovementTime<= 1)
+        {
+            currentMovementTime += Time.deltaTime/totalMovementTime;
+            mainCamera.transform.position = Vector3.Lerp(startPos, destination, Mathf.SmoothStep(0f, 1f, currentMovementTime));
+            yield return null;
+            
+        }
+        
+       
     }
 
     void EndBattle()
@@ -281,7 +406,7 @@ public class battleManager : MonoBehaviour
 
     public void removeAllBuffsFromPlayer()
     {
-        foreach(Character character in playerCharacter)
+        foreach (Character character in playerCharacter)
         {
             character.removeBuffsandDebuffs();
         }
